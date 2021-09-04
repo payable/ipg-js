827e25ea24a58e03013740cbfd696edebc3ce70a
Aslam Anver <slam.anver9@gmail.com>
2021-09-04 18:54:25 +0530

    after init

---
+++ react-demo/src/App.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import payable from '@payable/ipg-js'
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [logs, setLogs] = useState([]);
+
+  const [notifyUrl, setNotifyUrl] = useState("https://yoursite.org/payment/notify");
+  const [returnUrl, setReturnUrl] = useState("https://yoursite.org/payment/return");
+  const [cancelUrl, setCancelUrl] = useState("https://yoursite.org/payment/cancel");
+
+  const console = {
+    log: (message) => {
+      window.console.log(message);
+      setLogs(prevState => [...prevState, message])
+    }
+  }
+
+  payable.sandboxMode = true
+
+  payable.onCompleted = function onCompleted(data) {
+    console.log("Payment Process Completed");
+  };
+
+  payable.onDismissed = function onDismissed() {
+    console.log("Payment Process Canceled");
+  };
+
+  payable.onError = function onError(error) {
+    if (error.code === 3009) { // field validation error
+      error.fields.forEach((field) => {
+        console.log(field.error)
+      });
+    }
+    if (error.code === 3008) {
+      console.log(error.error)
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <input placeholder="notify_url" onChange={e => setNotifyUrl(e.target.value)} value={notifyUrl} type="text" />
+        <input placeholder="return_url" onChange={e => setReturnUrl(e.target.value)} value={returnUrl} type="text" />
+        <input placeholder="cancel_url" onChange={e => setCancelUrl(e.target.value)} value={cancelUrl} type="text" />
+        <button onClick={callPayable}>TEST</button>
+        <div className="logs">
+          {logs.map((item, key) => {
+            return <div key={key}>{key + 1}. {item}</div>;
+          })}
+        </div>
+      </header>
+    </div >
+  );
+
+  function callPayable() {
+
+    console.log('callPayable')
+
+    var payment = {
+      cancel_url: cancelUrl,
+      return_url: returnUrl,
+      notify_url: notifyUrl,
+      merchant_key: "D75BDF7DD35AD111",
+      check_value: "08375E2A654B423176C7FBED52B5907985BED78E8810BAAA0655B10CF37A58B3142C9D203259A9782452FEE721B065AD95F9AD1E3EA65C641D13F2477A376492",
+      amount: "1000.60",
+      invoice_id: "INV120306",
+      order_description: "Payment for ABC Fashion Shop",
+      currency_code: "LKR",
+      custom_1: "customYuDSFk5Z1O",
+      custom_2: "test2",
+      customer_email: "testmail@gmail.com",
+      customer_first_name: "Shakthi",
+      customer_last_name: "Elon",
+      customer_mobile_phone: "0774173844",
+      customer_phone: "0774173844",
+      billing_address_city: "Vavuniya",
+      billing_address_company: "Pay Shop",
+      billing_address_country: "LKA",
+      billing_address_postcode: "43000",
+      billing_address_province: "North Province",
+      billing_address_street: "154",
+      billing_address_street2: "Koomankulam",
+      shipping_address_city: "Colombo",
+      shipping_address_company: "Payable",
+      shipping_address_country: "LKA",
+      shipping_address_postcode: "43000",
+      shipping_address_province: "western province",
+      shipping_address_street: "Main street",
+      shipping_address_street2: "Temple road",
+      shipping_contact_email: "testshipmail@gmail.com",
+      shipping_contact_first_name: "Kumaran",
+      shipping_contact_last_name: "Test Lastname",
+      shipping_contact_mobile: "0774173844",
+      shipping_contact_phone: "0774173844",
+    };
+
+    payable.startPayment(payment);
+  }
+}
+
+export default App;
---
+++ test/tests.js
@@ -0,0 +1,11 @@
+const assert = require('assert')
+const payable = require('../index')
+const { rejects } = require('assert')
+
+describe("PAYable SDK", function () {
+
+	it("validation test", async function () {
+		assert.equal(0, 0)
+	})
+
+})
